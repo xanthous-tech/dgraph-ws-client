a70de87fa51dbd643097445f5ce7d7354f9715f5
Simon Liang <no.lhr0909@gmail.com>
2020-10-20 14:53:49 +0800

    added txn id

--- src/connections.rs
+++ src/connections.rs
@@ -41,16 +41,27 @@ pub async fn accept_mutate_txn_connection<M>(
     txn_arc_mutex: Arc<Mutex<Option<M>>>,
     shutdown_hook_arc_mutex: Arc<Mutex<Option<oneshot::Sender<()>>>>,
     query_count: Arc<AtomicU32>,
+    txn_id: String,
 ) where
     M: Mutate + TxnContextExport + 'static,
 {
     while let Some(message) = receiver.next().await {
+        let id = txn_id.clone();
         let sam = sender_arc_mutex.clone();
         let tam = txn_arc_mutex.clone();
         let sham = shutdown_hook_arc_mutex.clone();
         let qc = query_count.clone();
 
-        tokio::spawn(async move { process_mutate_message(sam, tam, sham, qc, message).await });
+        tokio::spawn(async move {
+            process_mutate_message(
+                sam,
+                tam,
+                sham,
+                qc,
+                message,
+                id.clone(),
+            ).await
+        });
     }
 }
 
@@ -187,6 +198,7 @@ async fn process_mutate_message<M>(
     shutdown_hook_arc_mutex: Arc<Mutex<Option<oneshot::Sender<()>>>>,
     query_count: Arc<AtomicU32>,
     message: Result<Message, Error>,
+    txn_id: String,
 ) where
     M: Mutate + TxnContextExport,
 {
@@ -296,9 +308,34 @@ async fn process_mutate_message<M>(
                                 .await
                             }
                             Err(err) => {
+                                let mut error_msg = Some(format!("{{\"message\": \"Txn Error: {:?}\"}}", &err));
+                                if err.is::<DgraphError>() {
+                                    let dgraph_err: DgraphError = err.downcast::<DgraphError>().unwrap();
+                                    match dgraph_err {
+                                        DgraphError::GrpcError(failure) => {
+                                            if failure.is::<ClientError>() {
+                                                let client_err: ClientError = failure.downcast::<ClientError>().unwrap();
+                                                match client_err {
+                                                    ClientError::CannotAlter(status)
+                                                     | ClientError::CannotCheckVersion(status)
+                                                     | ClientError::CannotCommitOrAbort(status)
+                                                     | ClientError::CannotDoRequest(status)
+                                                     | ClientError::CannotLogin(status)
+                                                     | ClientError::CannotMutate(status)
+                                                     | ClientError::CannotQuery(status)
+                                                     | ClientError::CannotRefreshLogin(status) => {
+                                                        error_msg.replace(format!("{{\"status\": {:}, \"message\": \"{:}\"}}", status.code(), status.message()));
+                                                    },
+                                                    _ => {},
+                                                };
+                                            }
+                                        },
+                                        _ => {},
+                                    };
+                                }
                                 let payload = ResponsePayload {
                                     id: request.id,
-                                    error: Some(format!("Txn Error: {:?}", err)),
+                                    error: error_msg,
                                     message: None,
                                     json: None,
                                     uids_map: None,
